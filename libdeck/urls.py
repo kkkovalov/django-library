aa1318ad5566b8f825d78969f3fd757563dbcd4f
kkkovalov <vlad_kovalov@icloud.com>
2023-07-03 19:00:27 -0700

    Adjusted a urlpatters for more dynamic apporoach. Finished Djangoproject tutorial 3 part.

--- libdeck/urls.py
+++ libdeck/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "libdeck"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:library_id>/", views.library, name="Library page"),
-    path("<int:library_id>/<int:book_id>/", views.books, name="Book page"),
+    path("<int:library_id>/", views.library, name="library_page"),
+    path("<int:library_id>/<int:book_id>/", views.books, name="book_page"),
 ]
