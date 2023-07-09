a0afc829e6ac2954acd03b4384666c28d6d5526b
kkkovalov <vlad_kovalov@icloud.com>
2023-07-08 18:46:16 -0700

    CRUD operation for the library complete. Still fails to have empty fields, esspecialy the datetime format

--- libdeck/urls.py
+++ libdeck/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:library_id>/", views.library, name="library_page"),
     path("<int:library_id>/<int:book_id>/", views.books, name="book_page"),
-    path("<int:library_id>/newbook/", views.newbook, name="new_book"),
+    path("<int:library_id>/newbook/", views.newBook, name="new_book"),
+    path("<int:library_id>/<int:book_id>/update/", views.updateBook, name="book_update"),
+    # path("<int:library_id>/<int:book_id>/deleteconfirmation/", views.deleteConfirmation, name="delete_confirmation"),
 ]
--- libdeck/views.py
+++ libdeck/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse, Http404, HttpRequest
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, Http404, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from datetime import datetime
 
 from .models import LibrarySpace, Book
 
@@ -9,6 +14,24 @@ def index(request):
     context = {"latest_library_list": latest_library_list}
     return render(request, "libdeck/index.html", context)
     # return HttpResponse("Welcome to library crossroads!");
+
+
+# class IndexView(generic.ListView):
+#     template_name = "libdeck/index.html"
+#     context_object_name = "latest_library_list"
+    
+#     def get_queryset(self):
+#         return LibrarySpace.objects.order_by("lib_date_opened")
+
+
+# class LibraryView(generic.ListView):
+#     model = LibrarySpace
+#     context_object_name = "latest_book_list"    
+#     template_name = "libdeck/library.html"
+# class BookView(generic.DeleteView):
+#     model = Book
+#     template_name="libdeck/book.html"
+
 
 def library(request, library_id):
     
@@ -26,24 +49,57 @@ def library(request, library_id):
     return render(request, "libdeck/library.html", context)
 
 def books(request, library_id, book_id):
-    try:
-        cur_book = Book.objects.get(pk=book_id)
-    except Book.DoesNotExist:
-        raise Http404("Book does not exist")
-    cur_library = LibrarySpace.objects.get(pk=library_id)
-    context = {"book": cur_book, "library": cur_library}
-    return render(request, "libdeck/book.html", context)
-
-def newbook(request, library_id):
     if request.method == "GET":
-        cur_library = get_object_or_404(LibrarySpace, pk=library_id)
+        try:
+            cur_book = Book.objects.get(pk=book_id)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+        cur_library = LibrarySpace.objects.get(pk=library_id)
+        context = {"book": cur_book, "library": cur_library}
+        return render(request, "libdeck/book.html", context)
+    elif "delete" in request.POST:
+        Book.objects.filter(pk=book_id).delete()
+        return HttpResponseRedirect(reverse("libdeck:library_page", args=(library_id,)))
+    elif "update" in request.POST:
+        return HttpResponseRedirect(reverse("libdeck:book_update", args=(library_id, book_id,)))
+    else:
+        raise Http404("The page you are trying to access does not exist.")
+
+def newBook(request, library_id):
+    cur_library = get_object_or_404(LibrarySpace, pk=library_id)
+    if request.method == "GET":
         context = {"library": cur_library}
         return render(request, "libdeck/newbook.html", context)
     elif request.method == "POST":
-        new_book = Book.objects.
-        return HttpResponse("Thank you for adding %s" % request.POST["book_name"])
-    
+        try:
+            new_book = Book(libdeck_id=cur_library, book_title=request.POST["book_title"], book_author=request.POST["book_author"], book_pub_date=request.POST["book_pub_date"], book_pages=request.POST["book_pages"])
+        except(KeyError, Book.DoesNotExist):
+            return render(request, "libdeck/newbook.html", context)
+        else:
+            new_book.save()
+            return HttpResponseRedirect(reverse("libdeck:book_page", args=(library_id,new_book.id,)))
     else:
         raise Http404("The page you are trying to access does not exist.")
         
+def updateBook(request, library_id, book_id):
+    cur_library = get_object_or_404(LibrarySpace, pk=library_id)
+    cur_book = get_object_or_404(Book, pk=book_id)
+    if request.method == "GET":
+        context = {"library": cur_library, "book": cur_book}
+        return render(request, "libdeck/updateBook.html", context)
+    elif request.method == "POST":
+        cur_book.book_title = request.POST.get("book_title", '')
+        cur_book.book_author = request.POST.get("book_author", '')
+        cur_book.book_pub_date = request.POST.get("book_pub_date")
+        cur_book.book_pages = request.POST.get("book_pages", 0)
+        cur_book.save()
+        return HttpResponseRedirect(reverse("libdeck:book_page", args=(library_id, book_id,)))
+    else:
+        raise Http404("The page you are trying to access does not exist.")
+
+# def deleteConfirmation(request, library_id, book_id):
+#     cur_library = get_object_or_404(LibrarySpace, pk=library_id)
+#     cur_book = get_object_or_404(Book, pk=book_id)
+#     context = {"library": cur_library, "book": cur_book}
+#     return render(request, "libdeck/deleteConfirmation.html", context)
     
